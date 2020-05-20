6773944134ba213ae5e9df0d4884cdfd124e67e4
xiaoxiong.li <nW8a33r)>
2020-05-20 18:39:14 +0800

    order-server init

--- order-start/src/main/java/com/bear/OrderApplication.java
+++ order-start/src/main/java/com/bear/OrderApplication.java
@@ -3,6 +3,8 @@ package com.bear;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @MapperScan
@@ -10,5 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
